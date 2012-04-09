7f65994e41618c4532e470c61df02ea62ea146fb
Roma Leventov <leventov@ya.ru>
2012-04-09 18:30:50 +0400

    cg lab 2 accepted

--- 2/cube.c
+++ 2/cube.c
@@ -18,7 +18,7 @@ int lcm (int a, int b) {
 	return a * b / gcd(a, b);
 }
 
-
+GLenum polyMode; 
 GLint phi = 0, psi = 45, aside = 1, top = 1;
 GLdouble PI = 3.14159265;
 
@@ -29,29 +29,76 @@ void reshape(int width, int height) {
 	w = width;
 }
 
+void crossProduct(double v1[3], double v2[3], double vr[3]) {
+    vr[0] = (v1[1]*v2[2])-(v2[1]*v1[2]);
+    vr[1] = (v1[2]*v2[0])-(v2[2]*v1[0]);
+    vr[2] = (v1[0]*v2[1])-(v1[1]*v2[0]);
+}
 
-void rdraw() {
-	glBegin(GL_TRIANGLES); 
-		glColor3f(1.0f,0.0f,0.0f);                      // Красный
-		glVertex3f( 0.0f, 1.0f, 0.0f);
-		glVertex3f(-1.0f,-0.5f, 1.0f);
-		glVertex3f( 0.7f,-1.0f, 1.0f); 
-		
-		glColor3f(0.0f,0.0f,1.0f);                      // Синий
-		glVertex3f( 0.3f, 0.1f, 0.2f);
-		glVertex3f( 0.6f,-1.0f, 1.0f);
-		glVertex3f( 1.0f,-1.0f, 0.8f);
-		
-		glColor3f(0.0f,1.0f,0.0f);                      // Зеленый
-		glVertex3f( 0.0f, -1.0f, -0.7f); 
-		glVertex3f( 1.0f, 0.9f, -1.0f);
-		glVertex3f(1.0f, 0.5f, 0.4f);
-	glEnd();
+void normalize(double v[3]) {
+	double c = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+	v[0] /= c;
+	v[1] /= c;
+	v[2] /= c;
+}
+
+#define vCount (20)
+#define lCount (15)
+double r = 1.0;
+
+double c[lCount][vCount][3];
+double *center, *rx, *ry; 
+void makeCircle(double mc[vCount][3]) {
+	for (int i = 0; i < vCount; i++) {
+		double a = i*2*PI/vCount;
+		double mx = r*cos(a), my = r*sin(a);
+		for (int j = 0; j < 3; j++)
+			mc[i][j] = center[j] + rx[j]*mx + ry[j]*my;
+	}
+}
+
+void initVertices() {
+	center = (double[]) {0, 0, 0};
+	rx = (double[]) {0, 1, 0};
+	ry = (double[]) {1, 0, 0};
+	double z, x;
+	for (int i = 0; i < lCount; i++) {
+		double a = i * PI / 90;
+		double dz = 1.0/lCount * cos(a);
+		double dx = 1.0/lCount * sin(a);
+		z += dz;
+		x += dz;
+		double vv[3] = {dx, 0, dz}, vl[3] = {-dz, 0, dx};
+		crossProduct(vv, vl, ry);
+		crossProduct(vv, ry, rx);
+		normalize(ry);
+		normalize(rx);
+		center[0] = x;
+		center[2] = z;
+		makeCircle(c[i]);
+	}
 }
 
 void draw() {
-	glutWireTeapot(0.5);
-	
+	for (int i = 0; i < lCount; i++) {
+		//glBegin(GL_LINE_LOOP);
+		//for (int j = 0; j < vCount; j++) {
+		//	glVertex3dv(c[i][j]);
+		//	//printf("%f, %f, %f\n", c[i][j][0], c[i][j][1], c[i][j][2]);
+		//} glEnd();
+		if (i > 0) {
+		glBegin(GL_QUAD_STRIP);
+			for (int j = 0; j < vCount; j++) {
+				glVertex3dv(c[i][j]);
+				glVertex3dv(c[i-1][j]);
+				glVertex3dv(c[i][(j+1)%vCount]);
+				
+				
+				glVertex3dv(c[i-1][(j+1)%vCount]);
+			}
+		glEnd();
+		}
+	}
 }
 
 void lookAt() {
@@ -100,7 +147,7 @@ double Z_P[16] = {
 void display() {
 	
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+	glPolygonMode(GL_FRONT_AND_BACK, polyMode);
 	// first view
 	glViewport (0, h/2, w/2, h/2); 
 	glMatrixMode(GL_PROJECTION);
@@ -114,13 +161,16 @@ void display() {
 	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
+	glScaled(0.5,0.5,0.5);
 	glRotated(90, 0,1,0);
+	
 	draw();
 	
 	
 	// second view
 	glViewport (w/2, h/2, w/2, h/2); 
 	glLoadIdentity();
+	glScaled(0.5,0.5,0.5);
 	glRotated(90, 1,0,0);
 	
 	draw();
@@ -129,13 +179,9 @@ void display() {
 	glViewport (0, 0, w/2, h/2); 
 	glLoadIdentity();
 	//glRotated(90, 0,0,1);
+	glScaled(0.5,0.5,0.5);
 	draw();
 	
-	glViewport (w/2, 0, w/2, h/2); 
-	glLoadIdentity();
-	glTranslated(0,0,5);
-	gluPerspective(45, 1, 0.1, 10);
-	draw();
 	glFlush();
 }
 
@@ -145,8 +191,9 @@ int init() {
 	glColor3f(0.1f, 0.4f, 0.4f);
 
 	glLineWidth(1.0);
-	
+	polyMode = GL_LINE;
 	//ec = lcm(100, rint(kr*100)) / 100;
+	initVertices();
 
 	return 1;
 }
@@ -198,6 +245,9 @@ void pressKey(unsigned char key, int x, int y) {
 		case 'x'://GLUT_KEY_DOWN: 
 			aside++;
 			break;
+		case 'f':
+			if (polyMode == GL_LINE) polyMode = GL_FILL;
+			else polyMode = GL_LINE;
 	}
 
 	//printf("%d %d\n", phi, psi);

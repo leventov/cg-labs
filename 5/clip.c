40ad400d7e1c768290783fff076aeded5894cee7
Roma Leventov <leventov@ya.ru>
2012-05-21 14:33:35 +0400

    5th accepted

--- 5/clip.c
+++ 5/clip.c
@@ -29,278 +29,317 @@ vector sub(vector *a, vector *b) {
 	return (vector) {.x = a->x - b->x, .y = a->y - b->y, .z = a->z - b->z};
 }
 
+vector add(vector *a, vector *b) {
+	return (vector) {.x = a->x + b->x, .y = a->y + b->y, .z = a->z + b->z};
+}
+
+vector sk(vector *a, double k) {
+	return (vector) {.x = a->x *= k, .y = a->y *= k, .z =  a->z *= k};
+}
+
+vector param(segment *s, double k) {
+	return (vector) {.x = s->p1.x * (1.0 - k) + s->p2.x * k,
+	                 .y = s->p1.y * (1.0 - k) + s->p2.y * k,
+	                 .z = s->p1.z * (1.0 - k) + s->p2.z * k
+	                };
+}
+
 vector *n, *f;
 int k;
 
 segment clip(segment *s) {
 	double t0 = 0, t1 = 1;
-	vector d = sub(&(s->p2),  &(s->p1));
+	double Ds, Ws, t;
+	vector ww;
+	vector d = sub(&(s->p2), &(s->p1));
 	for (int i = 0; i < k; i++) {
-		w = sub(&(s->p1), f + i);
-		double Ds = sProduct(d, n + i);
-		double Ws = sProduct(w, n + i);
-		if (zero(Ds)); //2
-		double t = -Ws/Ds;
-		if (Ds > 0.0); //1
-		if (t < 0.0); // 4
+		ww = sub(&(s->p1), f + i);
+		Ds = sProduct(&d, n + i);
+		Ws = sProduct(&ww, n + i);
+		if (zero(Ds)) goto m2; //2
+		t = -Ws/Ds;
+		if (Ds > 0.0) goto m1; //1
+		if (t < 0.0) goto m4; // 4
 		if (t < t1) t1 = t;
 		continue;
 		m1:
-		if (t > 1.0); //4
+		if (t > 1.0) goto m4; //4
 		if (t > t0) t0 = t;
 		continue;
 		m2:
-		if (Ws < 0.0); //4
+		if (Ws < 0.0) goto m4; //4
 	}
 	
 	m4:
-	return 
+	printf("clip: %g %g\n", t0, t1);
+	return (segment) {param(s, t0), param(s, t1)};
 }
 
-void edgeInsert(int x, int line /* y */, int vertex) {
-
-	int imin = 0, imax = edges.sizes[line];
-
-	if (!imax) {
-
-		edges.lines[line*LINES + 0] = x;
-
-		edges.sizes[line]++;
-		return;
-	}
-
-	imax--;
-
-	while (imax > imin) {
-		int imid = (imin + imax) / 2;
-		if (edges.lines[line*LINES + imid] < x)
-			imin = imid + 1;
-		else
-			imax = imid;
-	}
-	printf("1\n");
-	if (vertex && imax == imin && edges.lines[line*LINES + imin] == x)
-		return; // unique points
-	else {
-		for (int pos = edges.sizes[line]; pos > imin + 1; pos--)
-			edges.lines[line*LINES + pos] = edges.lines[line*LINES + pos - 1];
-		
-		if (edges.lines[line*LINES + imin] <= x)
-			edges.lines[line*LINES + imin + 1] = x;
-		else {
-			edges.lines[line*LINES + imin + 1] = edges.lines[line*LINES + imin];
-			edges.lines[line*LINES + imin] = x;
-		}
-		edges.sizes[line]++;
-	}
-	printf("1\n");
+int gcd (int a, int b) {
+	if (a == 1 || a == 0) return b;
+	if (b == 1 || b == 0) return a;
+	return gcd(b, a % b);
 }
 
-// Brezenham
-void drawLine(int x1, int y1, int x2, int y2) {
-	const int deltaX = abs(x2 - x1);
-	const int deltaY = abs(y2 - y1);
-	const int signX = x1 < x2 ? 1 : -1;
-	const int signY = y1 < y2 ? 1 : -1;
-	//
-	int error = deltaX - deltaY;
-	//
-	int prevY = y2;
-	int v = 0;
-	if (!((y2 < y1 && lly < y1) || (y2 > y1 && lly > y1))) {
-		//addVertex(x1, y1);
-		v = 1;
-		//printf("%d %d %d\n", lly, y1, y2);
-	}
-	
-	edgeInsert(x2, y2, 0);
-	putRaw(x2, y2);
-	edgeInsert(x1, y1, v);
-	putRaw(x1, y1);
-	int y11 = y1;
-	while(x1 != x2 || y1 != y2) {
-		if (prevY != y1 && y1 != y2 && y1 != y11) {
-			edgeInsert(x1, y1,0);
-			prevY =y1;
-		}
-		putRaw(x1, y1);
-		
-		const int error2 = error * 2;
-		//
-		if(error2 > -deltaY) {
-			error -= deltaY;
-			x1 += signX;
-		}
-		if(error2 < deltaX) {
-			error += deltaX;
-			y1 += signY;
-		}
-	}
+int lcm (int a, int b) {
+	return a * b / gcd(a, b);
 }
 
-void addEdgeTo(int x, int y) {
-	if (!vCount) {
-		fx = x;
-		fy = y;
-	} else {
-		// Не добавлять горизонтальные ребра в построчный скан
-		if (ly == y && lx != x) {
-			int minx, maxx;
-			if (x < lx) minx = x, maxx = lx;
-			else minx = lx, maxx = x;
-			for (int i = minx; i < maxx; i++)
-				putRaw(i, y);
-		} else
-			drawLine(lx, ly, x, y);
+GLenum polyMode; 
+GLint phi = 0, psi = 45, aside = 1, top = 1;
+GLdouble PI = 3.14159265;
+
+GLint line_phi = 0, line_psi = 90;
+segment init_seg;
+segment res_seg;
+
+int h, w;
+
+void recountSeg() {
+	double rpsi = PI*line_psi/180;
+	double rphi = PI*line_phi/180;
+	init_seg.p1 = (vector) {-1, 0, 0};
+	init_seg.p2.x = cos(rphi);
+	init_seg.p2.y = sin(rphi);
+	init_seg.p2.z = cos(rpsi);
+	printf("init_end: %g %g %g\n", init_seg.p2.x, init_seg.p2.y, init_seg.p2.z);
+	res_seg = clip(&init_seg);
+}
+
+void reshape(int width, int height) {
+	h = height;
+	w = width;
+}
+
+void crossProduct(double v1[3], double v2[3], double vr[3]) {
+    vr[0] = (v1[1]*v2[2])-(v2[1]*v1[2]);
+    vr[1] = (v1[2]*v2[0])-(v2[2]*v1[0]);
+    vr[2] = (v1[0]*v2[1])-(v1[1]*v2[0]);
+}
+
+void normalize(double v[3]) {
+	double c = sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+	v[0] /= c;
+	v[1] /= c;
+	v[2] /= c;
+}
+
+#define vCount (4)
+#define lCount (2)
+double r = 1.0;
+
+double c[lCount][vCount][3] = {
+	{{-0.5, -0.5, -0.5}, {-0.5, 0.5, -0.5}, 
+	{-0.5, 0.5, 0.5}, {-0.5, -0.5, 0.5}},
+	{{0.5, -0.5, -0.5}, {0.5, 0.5, -0.5}, 
+	{0.5, 0.5, 0.5}, {0.5, -0.5, 0.5}}
+};
+double *center, *rx, *ry; 
+
+
+void initClip() {
+	k = 6;
+	n = (vector *) malloc(6 * sizeof(vector));
+	f = (vector *) malloc(6 * sizeof(vector));
+	for (int i = 0; i  < 6; i++) {
+		*(f + i) = i < 3 ? (vector) {-0.5, -0.5, -0.5} : (vector) {0.5, 0.5, 0.5};
 	}
-	llx = lx;
-	lly = ly;
-	lx = x;
-	ly = y;
-	vCount++;
-}
-
-
-void fill() {
-	addEdgeTo(fx, fy);
-	
-	for (int y = 0; y < H; y++)
-		for (int j = 0; j < edges.sizes[y] - 1; j+=2) {
-//			int v1 = 0, v2 = 0;
-//			for (int k = 0; k < edges.vsizes[y]; k++) {
-//				if (edges.lines[y][j] == edges.vertices[y][k]) {
-//					v1 = 1;
-//				}
-//				if (edges.lines[y][j+1] == edges.vertices[y][k]) {
-//					v2 = 1;
-//				}
-//				if (v1 || v2) break;
-//			}
-//			if (v1) {
-//				j--; // to plus 1 in sum with 
-//				continue;
-//			}
-			for (int x = edges.lines[y*LINES + j];
-				x < edges.lines[y*LINES + j + 1]; 
-				x++)
-				putRaw(x, y);
-//			if (v2) j++;
-		}
-}
-
-void filterBuffer() {
-	double vs[] = {16.0, 12.0, 9.0};
-	
-	for (int i = 0; i < H; i++)
-		for (int j = 0; j < W; j++) {
-			int mini = -1, minj = -1, maxi = 2, maxj = 2;		
-			int vc = 0;
-			if (!i) mini = 1, vc++;
-			if (!j) minj = 1, vc++;
-			if (i == H - 1) maxi = -1, vc++;
-			if (j == W - 1) maxj = -1, vc++;
-			
-			FullPixel a = {0, 0, 0};
-			
-			for (int di = mini; di < maxi; di += 2)
-				for (int dj = minj; dj < maxj; dj += 2)
-					add(&a, &rawBuffer.pixels[(i + di)*W + j + dj], 1);
-
-			for (int di = mini; di < maxi; di += 2)
-				add(&a, &rawBuffer.pixels[(i + di)*W + j], 2);
-
-			for (int dj = minj; dj < maxj; dj += 2)
-				add(&a, &rawBuffer.pixels[(i)*W + j + dj], 2);
-			
-			add(&a, &rawBuffer.pixels[i*W + j], 4);
-			
-			//if (i == 2 && j == 2 )printf("%d %d %d ", a.red, a.green, a.blue);
-			divide(&a, vs[vc]);
-			//if (i == 2 && j == 2 )printf("%d %d %d\n", a.red, a.green, a.blue);
-			
-			toPixel(&a, &filteredBuffer.pixels[i*W + j]);
-		}
+	*n = (vector) {1, 0, 0};
+	*(n + 1) = (vector){0, 1, 0};
+	*(n + 2) = (vector){0, 0, 1};
+	*(n + 3) = (vector){0, -1, 0};
+	*(n + 4) = (vector){0, 0, -1};
+	*(n + 5) = (vector){-1, 0, 0};
 }
 
 void draw() {
-	if (smooth)
-		glDrawPixels(W, H, GL_RGB, GL_UNSIGNED_BYTE, filteredBuffer.pixels);
-	else
-		glDrawPixels(W, H, GL_RGB, GL_UNSIGNED_BYTE, rawBuffer.pixels);
+	for (int i = 0; i < lCount; i++) {
+		if (i > 0) {
+		glBegin(GL_QUAD_STRIP);
+			for (int j = 0; j < vCount; j++) {
+				glVertex3dv(c[i][j]);
+				glVertex3dv(c[i-1][j]);
+				glVertex3dv(c[i][(j+1)%vCount]);
+				
+				
+				glVertex3dv(c[i-1][(j+1)%vCount]);
+			}
+		glEnd();
+		}
+	}
+	
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3d(init_seg.p1.x, init_seg.p1.y, init_seg.p1.z);
+	glVertex3d(res_seg.p1.x, res_seg.p1.y, res_seg.p1.z);
+	
+	glColor3f(0.1f, 0.4f, 0.4f);
+	glVertex3d(res_seg.p1.x, res_seg.p1.y, res_seg.p1.z);
+	glVertex3d(res_seg.p2.x, res_seg.p2.y, res_seg.p2.z);
+	
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3d(res_seg.p2.x, res_seg.p2.y, res_seg.p2.z);
+	glVertex3d(init_seg.p2.x, init_seg.p2.y, init_seg.p2.z);
+	glColor3f(0.1f, 0.4f, 0.4f);
+	glEnd();
 }
 
+void lookAt() {
+	double rpsi = PI*psi/180;
+	double rphi = PI*phi/180;
+	double eye_z = sin(rpsi);
+	double xy = cos(rpsi);
+	double eye_x = xy * cos(rphi);
+	double eye_y = xy * sin(rphi); //sqrt(xy*xy - eye_x*eye_x);
+	//gluLookAt(eye_x, eye_y, eye_z, -eye_x, -eye_y, -eye_z, 0, 0, top);
+	glRotated(phi, 0, 0, 1);
+	glRotated(psi, 1, 0, 0);
+}
+
+int angle = 0, x = 0, y = 1, z = 0;
+
+double ID[16] = {
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1
+};
+
+
+double X_P[16] = {
+	0, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1
+};
+
+double Y_P[16] = {
+	1, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1
+};
+
+double Z_P[16] = {
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 1
+};
+
 void display() {
+	
 	glClear(GL_COLOR_BUFFER_BIT);
-	glReadBuffer(GL_FRONT);
+	glPolygonMode(GL_FRONT_AND_BACK, polyMode);
+	// first view
+	glViewport (0, h/2, w/2, h/2); 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//glOrtho(-2, 2, -2, 2, -2, 2);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	lookAt();
+	
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glScaled(0.5,0.5,0.5);
+	glRotated(90, 0,1,0);
+	
 	draw();
+	
+	
+	// second view
+	glViewport (w/2, h/2, w/2, h/2); 
+	glLoadIdentity();
+	glScaled(0.5,0.5,0.5);
+	glRotated(90, 1,0,0);
+	
+	draw();
+	
+	// third
+	glViewport (0, 0, w/2, h/2); 
+	glLoadIdentity();
+	//glRotated(90, 0,0,1);
+	glScaled(0.5,0.5,0.5);
+	draw();
+	
 	glFlush();
 }
 
-void init() {
-	rawBuffer.pixels = (Pixel*) calloc(H*W*3, 1);
-	
-	memset(rawBuffer.pixels, 255U, H*W*3);
-	
-	
-	filteredBuffer.pixels = (Pixel*) calloc(H*W*3, 1);
-	
-	//memset(&edges.lines, 0, H*LINES*4);
-	//memset(&edges.vertices, 0, H*LINES*4);
-	//memset(&edges.sizes, 0, H*4);
-	//memset(&edges.vsizes, 0, H*4);
-	
-	edges.lines =  (int*) calloc(H, LINES*4);
-	edges.sizes = (int*) calloc(H, 4);
-	smooth = 0;
-	filled = 0;
-	vCount = 0;
+int init() {                                      
+	glClearColor(0.9f, 0.9f, 0.9f, 0.9f);
+	glPointSize(2.0);
+	glColor3f(0.1f, 0.4f, 0.4f);
+
+	glLineWidth(1.0);
+	polyMode = GL_LINE;
+	//ec = lcm(100, rint(kr*100)) / 100;
+	initClip();
+
+	return 1;
 }
 
-void mouseClicked(int button, int state, int x, int y) {
-	if (!filled && state == GLUT_DOWN) {
-		addEdgeTo(x, H - y);
-		glutPostRedisplay();
-	}
+GLint direct = 1;
+
+void mouse(int button, int state, int x, int y) {
+	direct = -direct;
 }
 
 void pressKey(unsigned char key, int x, int y) {
 	switch (key) {
-		case 'f':
-			if (!filled)
-				fill();
-			filled = 1;
+		case 'a':
+			phi++;
 			break;
+		case 'd':
+			phi--;
+			break;
+			
 		case 's':
-			smooth = 1 - smooth;
-			if (smooth)
-				filterBuffer();
+			psi--;
 			break;
-		case 'c':
-			init();
+		case 'w':
+			psi++;
+			break;
+		case 'z':
+			if (aside > 1)
+				aside--;
+			break;
+		case 'x':
+			aside++;
+			break;
+		case 'f':
+			if (polyMode == GL_LINE) polyMode = GL_FILL;
+			else polyMode = GL_LINE;
+			
+		case 'k':
+			line_psi--;
+			recountSeg();
+			break;
+		case 'i':
+			line_psi++;
+			recountSeg();
 			break;
 	}
+	
 	glutPostRedisplay();
-}
+ }
 
-void reshape(int w, int h) {
-	H = h; W = w;
-	init();
-}
-
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[] ) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-	glutInitWindowSize(W, H);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("OpenGL lab 2");
 	
-	init();
-	
-	glutCreateWindow("OpenGL lab 5");
-	glutMouseFunc(mouseClicked);
+	glutReshapeFunc(reshape);
 	glutDisplayFunc(display);
 	glutKeyboardFunc(pressKey);
-	glutReshapeFunc(reshape);
+	//glutKeyboardUpFunc(releaseKey);
+	//glutMouseFunc(mouse);
+	//glutTimerFunc(100, timf, 0); // Set up timer for 40ms, about 25 fps
 	
+	init();
 	glutMainLoop();
 	return 0;
 }

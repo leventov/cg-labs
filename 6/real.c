c247af3d61321191ae89774dd0e6ac9a21daa4bc
Roma Leventov <leventov@ya.ru>
2012-05-28 17:41:08 +0400

    dgfg

--- 6/real.c
+++ 6/real.c
@@ -22,6 +22,11 @@ GLenum polyMode;
 GLint phi = 0, psi = 45, aside = 1, top = 1;
 GLdouble PI = 3.14159265;
 
+#define WIND_STEP (0.001)
+GLdouble wind = WIND_STEP;
+GLdouble direction[3] = {0.4, 0.3, 0.5};
+GLdouble position[3] = {0.0, 0.0, 0.0};
+
 int h, w;
 
 void reshape(int width, int height) {
@@ -79,22 +84,87 @@ void initVertices() {
 	}
 }
 
+#define BOUND_SIDE (4.0)
+
+GLdouble cube[8][3] = {
+	{-BOUND_SIDE/2, -BOUND_SIDE/2, -BOUND_SIDE/2},
+	{-BOUND_SIDE/2, -BOUND_SIDE/2, BOUND_SIDE/2},
+	
+	{BOUND_SIDE/2, -BOUND_SIDE/2, -BOUND_SIDE/2},
+	{BOUND_SIDE/2, -BOUND_SIDE/2, BOUND_SIDE/2},
+	
+	{BOUND_SIDE/2, BOUND_SIDE/2, -BOUND_SIDE/2},
+	{BOUND_SIDE/2, BOUND_SIDE/2, BOUND_SIDE/2},
+	
+	{-BOUND_SIDE/2, BOUND_SIDE/2, -BOUND_SIDE/2},
+	{-BOUND_SIDE/2, BOUND_SIDE/2, BOUND_SIDE/2}
+};
+
+void draw_cube() {
+	//glColor3f(0.9f, 0.9f, 0.9f);
+	glBegin(GL_LINES);
+	for (int i = 0; i < 4; i++) {
+		glVertex3dv(cube[i*2]);
+		glVertex3dv(cube[i*2+1]);
+	}
+	for (int i = 0; i < 4; i++) {
+		glVertex3dv(cube[i*2]);
+		glVertex3dv(cube[(i*2+2)%8]);
+		glVertex3dv(cube[i*2+1]);
+		glVertex3dv(cube[(i*2+3)%8]);
+	}
+	glEnd();
+	//glColor()
+}
+
 void draw() {
+	draw_cube();
 	for (int i = 0; i < lCount; i++) {
 		//glBegin(GL_LINE_LOOP);
 		//for (int j = 0; j < vCount; j++) {
 		//	glVertex3dv(c[i][j]);
 		//	//printf("%f, %f, %f\n", c[i][j][0], c[i][j][1], c[i][j][2]);
 		//} glEnd();
+		int reflected = 0;
 		if (i > 0) {
 		glBegin(GL_QUAD_STRIP);
 			for (int j = 0; j < vCount; j++) {
-				glVertex3dv(c[i][j]);
-				glVertex3dv(c[i-1][j]);
-				glVertex3dv(c[i][(j+1)%vCount]);
+				glVertex3d(c[i][j][0]+position[0], c[i][j][1] + position[1], c[i][j][2]+position[2]);
+				glVertex3d(c[i-1][j][0]+position[0], c[i-1][j][1] + position[1], c[i-1][j][2]+position[2]);
+				
+				glVertex3d(c[i][(j+1)%vCount][0] + position[0], 
+						   c[i][(j+1)%vCount][1] + position[1], 
+						   c[i][(j+1)%vCount][2] + position[2]);
+				
+				glVertex3d(c[i-1][(j+1)%vCount][0] + position[0], 
+						   c[i-1][(j+1)%vCount][1] + position[1], 
+						   c[i-1][(j+1)%vCount][2] + position[2]);
+				if (reflected == 0) {
+					if (abs(position[0]+c[i][j][0]) >= 2.0) {
+						if (direction[0] * wind * (position[0]+c[i][j][0]) > 0) {
+							direction[0] = -direction[0];
+							reflected = 1;
+						}
+					}
+					else if (abs(position[1]+c[i][j][1]) >= 2.0) {
+						if (direction[1] * wind * (position[1]+c[i][j][1]) > 0) {
+							direction[1] = -direction[1];
+							reflected = 1;
+						}
+					}
+					else if (abs(position[2]+c[i][j][2]) >= 2.0) {
+						if (direction[2] * wind * (position[2]+c[i][j][2]) > 0) {
+							direction[2] = -direction[2];
+							reflected = 1;
+						}
+					}
+				}
+				
+				//glVertex3dv(c[i-1][j]);
+				//glVertex3dv(c[i][(j+1)%vCount]);
 				
 				
-				glVertex3dv(c[i-1][(j+1)%vCount]);
+				//glVertex3dv(c[i-1][(j+1)%vCount]);
 			}
 		glEnd();
 		}
@@ -144,10 +214,37 @@ double Z_P[16] = {
 	0, 0, 0, 1
 };
 
+float diffuse[] = {1.0, 1.0, 1.0,1.0};
+float ambient[] = {0.5, 0.5, 0.5, 1};
+
 void display() {
+	glPushMatrix();
+		glTranslated(7.0, 7.0, 7.0);//координаты лампочки
+		
+		glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.9);
+		glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.9);
+		glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, 0.9); //рассеивание устанавливается квадратичное
+		
+		GLfloat light_pos[] = { 0.0, 0.0, 0.0, 1.0 };
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos); //вектор источника света, источник - лампочка
+		glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+		
+		//glDisable(GL_LIGHTING);
+		
+		//рисуем источник света
+		//printf("%d %d %d\n", red, green, blue);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	//	glutSolidSphere(0.1, 10, 10);
+		
+		//glEnable(GL_LIGHTING);
+	glPopMatrix();
+	
+	glEnable(GL_TEXTURE_2D);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 	
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPolygonMode(GL_FRONT_AND_BACK, polyMode);
+	
 	// first view
 	glViewport (0, h/2, w/2, h/2); 
 	glMatrixMode(GL_PROJECTION);
@@ -161,7 +258,7 @@ void display() {
 	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glScaled(0.5,0.5,0.5);
+	glScaled(0.3,0.3,0.3);
 	glRotated(90, 0,1,0);
 	
 	draw();
@@ -170,7 +267,7 @@ void display() {
 	// second view
 	glViewport (w/2, h/2, w/2, h/2); 
 	glLoadIdentity();
-	glScaled(0.5,0.5,0.5);
+	glScaled(0.3,0.3,0.3);
 	glRotated(90, 1,0,0);
 	
 	draw();
@@ -179,7 +276,7 @@ void display() {
 	glViewport (0, 0, w/2, h/2); 
 	glLoadIdentity();
 	//glRotated(90, 0,0,1);
-	glScaled(0.5,0.5,0.5);
+	glScaled(0.3,0.3,0.3);
 	draw();
 	
 	glFlush();
@@ -189,6 +286,11 @@ int init() {
 	glClearColor(0.9f, 0.9f, 0.9f, 0.9f);
 	glPointSize(2.0);
 	glColor3f(0.1f, 0.4f, 0.4f);
+	
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
 
 	glLineWidth(1.0);
 	polyMode = GL_LINE;
@@ -205,20 +307,41 @@ void mouse(int button, int state, int x, int y) {
 }
 
 
-/*
+
 void timf(int v) {
-	//offset_a += 190;
 	glutPostRedisplay();
+	/*
+	//offset_a += 190;
+	
 	
 	offset_a = offset_a +  1 * direct * PI / 360;
 	
 	printf("%f", offset_a);
-	glutTimerFunc( 100, timf, 0 );
+	*/
+	position[0] += direction[0] * wind;
+	position[1] += direction[1] * wind;
+	position[2] += direction[2] * wind;
+	glutTimerFunc( 10, timf, 0 );
 }
-*/
+
 
 void pressKey(unsigned char key, int x, int y) {
 	switch (key) {
+		case 'r':
+			//diffuse[0] = (diffuse[0] + 1) % 10;
+			break;
+		case 'g':
+			//diffuse[1] = (diffuse[1] + 1) % 10;
+			break;
+		case 'b':
+			//diffuse[2] = (diffuse[2] + 1) % 10;
+			break;
+		case 'm':
+			wind += WIND_STEP;
+			break;
+		case 'n':
+			wind -= WIND_STEP;
+			break;
 		case 'a'://GLUT_KEY_LEFT: 
 			phi++;
 			break;
@@ -265,7 +388,7 @@ int main(int argc, char *argv[] ) {
 	glutKeyboardFunc(pressKey);
 	//glutKeyboardUpFunc(releaseKey);
 	//glutMouseFunc(mouse);
-	//glutTimerFunc(100, timf, 0); // Set up timer for 40ms, about 25 fps
+	glutTimerFunc(10, timf, 0); // Set up timer for 40ms, about 25 fps
 	
 	init();
 	glutMainLoop();
